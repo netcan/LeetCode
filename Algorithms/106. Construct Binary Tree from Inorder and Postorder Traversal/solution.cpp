56495365bf4a4bb5ef61ffcf3700777d91fc1043
netcan <1469709759@qq.com>
2017-01-13 12:53:39 +0800

    中序后序遍历构造二叉树

--- Algorithms/106. Construct Binary Tree from Inorder and Postorder Traversal/solution.cpp
+++ Algorithms/106. Construct Binary Tree from Inorder and Postorder Traversal/solution.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 // Definition for a binary tree node.
@@ -17,29 +18,28 @@ struct TreeNode {
 	TreeNode *right;
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
+
 class Solution {
 	public:
 		// 中序后序遍历构造二叉树
 		// [4,6,2,1,3,5]
 		// [6,4,2,5,3,1]
-		TreeNode* build(vector<int>& inorder, vector<int>& postorder, int iStart, int iEnd, int pStart, int pEnd) { // [iStart, iNnd) [pStart, pEnd)
+		TreeNode* build(unordered_map<int, int>& hash, vector<int>& inorder, vector<int>& postorder, int iStart, int iEnd, int pStart, int pEnd) { // [iStart, iNnd) [pStart, pEnd)
 			TreeNode *p = new TreeNode(postorder[pEnd - 1]);
 			if(pStart + 1 == pEnd) return p;
 			else if(pStart >= pEnd || iStart >= iEnd) return NULL;
-			int mid = 0;
-			while(inorder[mid] != postorder[pEnd - 1]) ++mid;
-			// printf("iStart = %d iEnd=%d mid=%d pStart=%d pEnd=%d\n", iStart, iEnd, mid, pStart, pEnd);
-
-			// puts("left");
-			p->left = build(inorder, postorder, iStart, mid, pStart, pEnd - iEnd + mid);
-			// puts("right");
-			p->right = build(inorder, postorder, mid + 1, iEnd, pEnd - iEnd + mid, pEnd-1);
+			int mid = hash[postorder[pEnd - 1]];
+			// for(mid=iStart; mid < iEnd && inorder[mid] != postorder[pEnd - 1]; ++mid);
+			p->left = build(hash, inorder, postorder, iStart, mid, pStart, pEnd - iEnd + mid);
+			p->right = build(hash, inorder, postorder, mid + 1, iEnd, pEnd - iEnd + mid, pEnd-1);
 			return p;
 		}
 		TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
 			int N = inorder.size();
 			if(! N) return NULL;
-			TreeNode *tree = build(inorder, postorder, 0, N, 0, N);
+			unordered_map<int, int> hash;
+			for (int i = 0; i < inorder.size(); ++i) hash[inorder[i]] = i;
+			TreeNode *tree = build(hash, inorder, postorder, 0, N, 0, N);
 			return tree;
 		}
 };
