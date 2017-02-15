2d71bb502e0a47d5a776f878f8e839e1efba6184
netcan <1469709759@qq.com>
2017-02-15 11:54:57 +0800

    链表去重

--- Algorithms/83. Remove Duplicates from Sorted List/solution.cpp
+++ Algorithms/83. Remove Duplicates from Sorted List/solution.cpp
@@ -3,7 +3,7 @@
 	  > Author: Netcan
 	  > Blog: http://www.netcan666.com
 	  > Mail: 1469709759@qq.com
-	  > Created Time: 2017-01-09 10:11
+	  > Created Time: 2017-02-15 11:37
  ************************************************************************/
 
 #include <iostream>
@@ -20,16 +20,28 @@ struct ListNode {
 class Solution {
 	public:
 		ListNode* deleteDuplicates(ListNode* head) {
-			if(! head) return head;
-			ListNode *p = head;
-			while(p && p->next) {
-				ListNode *q = p->next;
-				if(p->val == q->val) {
-					p->next = q->next;
-					delete q;
+			if(head == NULL || head->next == NULL) return head;
+			ListNode **p = &head;
+			bool isDupclidate = false;
+			while(*p) {
+				if((*p)->next) {
+					if((*p)->val == (*p)->next->val || isDupclidate) {
+						if((*p)->val == (*p)->next->val)
+							isDupclidate = true;
+						else isDupclidate = false;
+						ListNode *t = (*p)->next;
+						**p = *(*p)->next;
+						delete t;
+					} else
+						p = &(*p)->next;
+				} else {
+					if(isDupclidate) {
+						ListNode *t = *p;
+						*p = NULL;
+						delete t;
+					}
+					break;
 				}
-				else
-					p = p->next;
 			}
 			return head;
 		}
